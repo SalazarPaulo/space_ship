34eb31400ef11bca72d40fb10ccdb34e85be2086
SalazarPaulo <pauloiisalazar@gmail.com>
2026-07-04 18:02:28 -0600

    feat: complete Space Ship arcade edition

--- src/project/Main.java
+++
@@ -1,8 +0,0 @@
-package src.project;
-
-public class Main {
-    public static void main (String x[]) {
-        Window obj = new Window();
-        obj.start(); // Arranca el juego, inicializa el thread 
-    }
-}
